a0d34d4937489bf24ebfccdb4cb39fc5033649a5
TalexDreamSoul <2418876761@qq.com>
2024-05-19 22:29:10 +0800

    修复页面颜色问题

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -14,13 +14,13 @@ import Account from './login/account.vue'
 .Login-Wrapper {
   position: absolute;
 
-
   top: 50%;
   left: 50%;
 
   width: 50%;
   min-height: 240px;
 
+  color: #fff;
   transform: translate(-50%, -50%);
 }
 </style>
